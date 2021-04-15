c424d4eaac179f8adc5a50484c6d5243a4a50a8c
s.gromov <zz-gromov@ya.ru>
2021-04-15 23:45:11 +0300

    sync

--- src/components/apiContainer/apiContainer.tsx
+++ src/components/apiContainer/apiContainer.tsx
@@ -4,17 +4,21 @@ import axios from "~/lib/axios";
 import { Pagination, Spin } from 'antd';
 import useSWR from 'swr'
 import {pageScrollUp} from '~/helpers/pageScrollUp'
+import { useHistory, useParams } from 'react-router-dom';
+import qs from 'qs'
 
 interface ApiContainerProps {
   url: string
 }
+
 const fetcher = (url:string) => axios.get(url).then(res => res.data)
 
-const _ApiContainer: FC<PropsWithChildren<ApiContainerProps>> = ({ children, url: propsUrl }) => {
+const _ApiContainer: FC<PropsWithChildren<ApiContainerProps>> = ({ children, url }) => {
   const [softData, setSoftData] = useState<any>()
-  const [url, setUrl] = useState(propsUrl)
-  const { data, error } = useSWR(url, fetcher)
+  const [apiUrl, setApiUrl] = useState(url)
+  const { data, error } = useSWR(apiUrl, fetcher)
 
+  
   useEffect(function () {
     if (!data) return
     setSoftData(data)
@@ -24,7 +28,14 @@ const _ApiContainer: FC<PropsWithChildren<ApiContainerProps>> = ({ children, url
 
   function paginationChange(page: number, pageSize?: number) {
     pageScrollUp()
-    setUrl(String(softData?.meta?.links[page]?.url || 0))
+    setApiUrl(`${url}?page=${page}`)
+  }
+
+  const defaultPagination = {
+    current: softData?.meta?.current_page,
+    total: softData?.meta?.total,
+    pageSize: softData?.meta?.per_page || 0,
+    onChange: paginationChange,
   }
 
   return (
@@ -36,10 +47,7 @@ const _ApiContainer: FC<PropsWithChildren<ApiContainerProps>> = ({ children, url
         { hasPagination() &&
           <PaginationWrap>
             <Pagination
-              current={softData?.meta?.current_page}
-              total={softData?.meta?.total}
-              pageSize={softData?.meta?.per_page || 0}
-              onChange={paginationChange}
+              { ...defaultPagination }
             />
           </PaginationWrap>
         }
