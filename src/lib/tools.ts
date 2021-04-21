0e127df52b6b78ed4f9cafc979c8d178ffce991e
s.gromov <zz-gromov@ya.ru>
2021-04-22 00:42:41 +0300

    sync

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -9,6 +9,7 @@ import LinkTool from '@editorjs/link'
 import Image from '@editorjs/image'
 import Raw from '@editorjs/raw'
 import Header from '@editorjs/header'
+import HeaderAnchor from 'editorjs-header-with-anchor'
 import Quote from '@editorjs/quote'
 import Marker from '@editorjs/marker'
 import CheckList from '@editorjs/checklist'
@@ -16,21 +17,54 @@ import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
 import SimpleImage from '@editorjs/simple-image'
 
+const  AlignmentTuneTool  =  require('editorjs-text-alignment-blocktune') ;
+
 export const EDITOR_JS_TOOLS = {
-  embed: Embed,
+  header: {
+    class: HeaderAnchor,
+    // tunes: ['anyTuneName']
+  },
+  anyTuneName: {
+    class:AlignmentTuneTool,
+    config:{
+      default: "right",
+      blocks: {
+        header: 'center',
+        // list: 'right'
+      }
+    }
+  },
+  embed: {
+    class: Embed,
+    services: {
+      youtube: true,
+      coub: true,
+      imgur: true,
+      gfycat: true,
+      'twitch-video': true,
+      'twitch-channel': true,
+      vimeo: true,
+      vine: true,
+      'yandex-music-track': true,
+      'yandex-music-album': true,
+      'yandex-music-playlist': true,
+      'twitter': true,
+      'instagram': true,
+    },
+    inlineToolbar: true
+  },
+  simpleImage: SimpleImage,
   table: Table,
+  image: Image,
   // paragraph: Paragraph,
   list: List,
-  warning: Warning,
   code: Code,
   linkTool: LinkTool,
-  // image: Image,
   raw: Raw,
-  header: Header,
   quote: Quote,
   marker: Marker,
   checklist: CheckList,
   delimiter: Delimiter,
   inlineCode: InlineCode,
-  simpleImage: SimpleImage
+  warning: Warning,
 }
